35c922160d5a6105de9cfacecb6ba8621480153e
Pavan <reddyvaripavan90@gmail.com>
2026-08-06 22:22:15 +0530

    Add files via upload

---
+++ p2.py
@@ -0,0 +1,33 @@
+# num=int(input('num:'))
+# sum_=0
+# for i in str(num):
+#   sum_+=int(i)
+# print(sum_)
+
+# # num:234
+# # 9
+
+
+# num=int(input('num:'))
+# sum_=0
+# while num!=0:
+#   last_digit=num%10
+#   if last_digit%2==0:sum_+=last_digit
+#   num//=10
+# print(sum_)
+
+# # num:1234
+# # 6
+
+# num=int(input('number:'))
+# while not 0<num<10:
+#   sum_=0
+#   while num!=0:
+#     ld=num%10
+#     sum_+=ld
+#     num=num//10
+#   num=sum_
+# print(num)
+
+# # number:24681
+# # 3
